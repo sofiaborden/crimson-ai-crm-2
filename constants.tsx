14808fbf5bf85e334f69fc6a360ff76882b12a8b
Sofia Borden <sofiaborden@Mac.localdomain>
2025-10-30 12:45:31 -0400

    Fix: Add missing EllipsisVerticalIcon to constants.tsx
    - Added EllipsisVerticalIcon export to constants.tsx
    - Resolves import error in SmartTagsManager2.tsx
    - Fixes blank page issue on Smart Tags 2 navigation
    - Icon matches Heroicons vertical ellipsis design

--- constants.tsx
+++ constants.tsx
@@ -494,6 +494,12 @@ export const EllipsisHorizontalIcon = (props: React.SVGProps<SVGSVGElement>) => 
   </svg>
 );
 
+export const EllipsisVerticalIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6" {...props}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+  </svg>
+);
+
 export const PlayIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6" {...props}>
     <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
